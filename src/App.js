e77fdca483b1c42926a64ea74748024b21c2a113
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2023-02-14 23:54:12 +0200

    update :#

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import './calendar.css';
-import MonthView1 from './components/MonthView';
+import MonthView from './components/MonthView';
 import {events} from './eventsData';
 function App() {
     return (
         <div className='App'>
-            <MonthView1 month={1} year={2023} events={events} />
+            <MonthView month={1} year={2023} events={events} />
         </div>
     );
 }
